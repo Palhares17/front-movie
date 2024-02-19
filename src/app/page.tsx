be2ab13217e97922d6e38d3dbe0a608ac70a5a5a
Palhares17 <mspalhares01@gmail.com>
2024-02-18 23:27:56 -0300

    Update image domains and fetch movies from API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,61 +2,47 @@ import { Header } from "@/components/header"
 import Image from "next/image";
 import { useState } from "react";
 
-export default function Home() {
+export async function getMovies() {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?language=pt-br&page=1`, {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNWMyODQ3ZTA5NjE1YjIwNjkxMzk5MjdkMWFlMzcwMSIsInN1YiI6IjY1ZDJiNDYzNGJjMzhiMDE4OTU0OTVmOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vj77mD6dt7PXUfYfzqs4ZFbJr1nlgvwc6IoWCaHmjiw'
+    }
+  });
+
+  const data = await response.json();
+
+  console.log(data);
+  return data;
+}
+
+export default async function Home() {
+  const movies = await getMovies();
+
   return (
     <>
       <Header />
       <main className="px-16 py-16">
         <h1 className="text-3xl font-bold">Todos os filmes </h1>
 
-        <ul className="grid grid-cols-3">
-          <li>
-            <div>
-              <Image
-                src="https://cdn.awsli.com.br/2500x2500/1610/1610163/produto/177685234/poster-os-vingadores-ultimato-e-e845a7aa.jpg"
-                alt="Os Vingadores: Ultimato"
-                width={200}
-                height={200}
-                className="mt-6 border- rounded-xl"
-              />
-              <p className="font-bold text-base mt-3">Vingadores</p>
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <Image
-                src="https://cdn.awsli.com.br/2500x2500/1610/1610163/produto/177685234/poster-os-vingadores-ultimato-e-e845a7aa.jpg"
-                alt="Os Vingadores: Ultimato"
-                width={200}
-                height={200}
-                className="mt-6 border- rounded-xl"
-              />
-              <p className="font-bold text-base mt-3">Vingadores</p>
-            </div>
-          </li>
-
-          <li>
-            <div>
-              <Image
-                src="https://cdn.awsli.com.br/2500x2500/1610/1610163/produto/177685234/poster-os-vingadores-ultimato-e-e845a7aa.jpg"
-                alt="Os Vingadores: Ultimato"
-                width={200}
-                height={200}
-                className="mt-6 border- rounded-xl"
-              />
-              <p className="font-bold text-base mt-3">Vingadores</p>
-            </div>
-          </li>
+        <ul className="grid grid-cols-6 gap-6">
+          {
+            movies.results.map((item: any) => (
+              <li key={item.id}>
+                <Image
+                  src={`https://image.tmdb.org/t/p/original${item.poster_path}`}
+                  alt={item.title}
+                  width={200}
+                  height={200}
+                  className="mt-6 border- rounded-xl"
+                />
+                <p className="font-bold text-base mt-3">{item.title}</p>
+              </li>
+            ))
+          }
         </ul>
       </main>
     </>
   );
-}
-
-export async function getServerSideProps() {
-  const response = await fetch(`https://...`)
-  const data = await response.json()
-
-  return { props: { data } }
 }
