2f87730295d6f9eadc356b609bf1f65dff08ac1b
Palhares17 <mspalhares01@gmail.com>
2024-05-07 23:28:20 -0300

    style: styled sidebar

--- src/components/ui/sidebar/index.tsx
+++ src/components/ui/sidebar/index.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import styles from './styles.module.css';
+import { SignIn } from '@phosphor-icons/react';
 
 export default function Sidebar({ animate }: { animate: boolean }) {
   return (
@@ -9,26 +10,32 @@ export default function Sidebar({ animate }: { animate: boolean }) {
       className={`${styles.container} ${animate ? styles.active : styles.desabled}`}
     >
       <nav className={styles.nav}>
-        <li className={styles.li}>
-          <Link href="/" className={styles.link}>
-            Início
-          </Link>
-        </li>
-        <li className={styles.li}>
-          <Link href="/movies" className={styles.link}>
-            Filmes
-          </Link>
-        </li>
-        <li className={styles.li}>
-          <Link href="/series" className={styles.link}>
-            Séries
-          </Link>
-        </li>
-        <li className={styles.li}>
-          <Link href="/mylist" className={styles.link}>
-            Minha lista
-          </Link>
-        </li>
+        <ul>
+          <li className={styles.li}>
+            <Link href="/" className={styles.link}>
+              Início
+            </Link>
+          </li>
+          <li className={styles.li}>
+            <Link href="/movies" className={styles.link}>
+              Filmes
+            </Link>
+          </li>
+          <li className={styles.li}>
+            <Link href="/series" className={styles.link}>
+              Séries
+            </Link>
+          </li>
+          <li className={styles.li}>
+            <Link href="/mylist" className={styles.link}>
+              Minha lista
+            </Link>
+          </li>
+        </ul>
+        <button className={styles.login}>
+          <SignIn size={24} />
+          Fazer Login
+        </button>
       </nav>
     </aside>
   );
