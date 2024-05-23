5e1a3527b3d468a0df8b9919ac8711418264d151
Palhares17 <mspalhares01@gmail.com>
2024-05-22 21:37:57 -0300

    bug: i dont know why the font isnt working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { Poppins } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/ui/header';
 import Footer from '@/components/ui/footer';
-
-const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
-const poppins = Poppins({
-  weight: ['400', '500', '700'],
-  subsets: ['latin'],
-  variable: '--font-poppins',
-  display: 'swap',
-});
+import { inter } from '@/fonts';
 
 export const metadata: Metadata = {
   title: 'Shfit Inc.',
@@ -24,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className={`${inter.variable} ${poppins.variable}`}>
-      <body className={`${inter.className} ${poppins.className}`}>
+    <html lang="pt-BR" className={`${inter.className}`}>
+      <body>
         <Header />
         {children}
         <Footer />
